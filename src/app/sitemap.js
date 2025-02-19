75ecb6c58372d425667f39e7d3cb0807faba23a9
korneXD <korcika2005@gmail.com>
2025-02-19 21:27:34 +0100

    sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,23 +1,24 @@
 import fs from "fs";
 import path from "path";
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
 async function getLastModifiedDate(filePath) {
   filePath = path.join(process.cwd(), filePath);
   const stats = await fs.promises.stat(filePath);
   return stats.mtime;
 }
 
-const BaseURL = process.env.NEXT_PUBLIC_BASE_URL;
-
 export default async function sitemap() {
-  const pageRoutes = [""];
+  const pageRoutes = ["", "arlista"];
   const staticRoutes = await Promise.all(
     pageRoutes.map(async (pageRoute) => {
       const lastModified = await getLastModifiedDate(
         `src/app/${pageRoute}/page.jsx`,
       );
+      const page = pageRoute.replace(/\/?\(.*?\)/g, "").replace(/^\/+/, "");
       return {
-        url: `${BaseURL}`,
+        url: `${BASE_URL}/${page}`,
         lastModified,
       };
     }),
