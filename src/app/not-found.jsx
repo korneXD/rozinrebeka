ceb39e6ab127527a1d4c6e025c438540dc784ab2
korneXD <korcika2005@gmail.com>
2025-02-20 08:36:41 +0100

    not found 404

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function NotFound() {
-  return <h1>nem</h1>;
+  return (
+    <section className="text-cappuccino flex min-h-screen flex-col items-center justify-center gap-2 font-serif">
+      <h2 className="text-4xl italic">- 404 -</h2>
+      <h2 className="text-4xl">Rozin Rebeka</h2>
+      <h3 className="text-2xl italic">
+        A felkeresett oldal nem található :{"("}
+      </h3>
+      <Link href={"/"} className="text-xl italic underline">
+        Vissza a Főoldalra
+      </Link>
+    </section>
+  );
 }
